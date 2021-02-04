86f87567505851bb6f1e802cc89f200a66153cac
Хэшка <68265022+XK-Vime@users.noreply.github.com>
2021-02-04 18:25:58 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+api_id = 1640000
+api_hash = '8741415ce4f8fd1370d72029b62fa0bd'
+session_name = 'session'
+
+image_filename = 'time_image.jpg'
+photo_filename = '/resources/photo.jpg'
+image_text_color = 'white' # '#ff0800'
+image_border_color = 'white'
+image_border_size = 3
+
+profile_bio = 'Прогресс дня: {}%'
